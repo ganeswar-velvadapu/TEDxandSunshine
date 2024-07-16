633536fb01d330a39b3ea1e122d123eaea8332d9
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2024-07-16 17:33:07 +0530

    Add Initial Files

---
+++ blog-site/app.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookiePaser = require("cookie-parser");
+
+const Blog = require("./models/blog");
+
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+
+const app = express();
+const PORT = 3000;
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/blog-site");
+}
+
+main()
+  .then(() => console.log("Connected to data base"))
+  .catch((err) => console.log(err));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookiePaser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+});
+
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
